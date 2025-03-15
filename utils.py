13f9792d517fa387b023cc1fc40c971e36983fec
khoa16122004 <khoatr.work@gmail.com>
2025-03-15 22:10:24 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -114,8 +114,10 @@ class Fitness:
         c_advs = img_2_cap(self.model, image_advs)
         c_adv_embeddings = self.encode_text(c_advs)
         adv_tar_sim = torch.sum(self.c_tar_embedding * c_adv_embeddings, dim=1)
-        adv_clean_sim = torch.sum(self.c_clean_embedding * self.c_tar_embedding, dim=1)
-        fitness_ = adv_tar_sim - adv_clean_sim
+        tar_clean_sim = torch.sum(self.c_clean_embedding * self.c_tar_embedding, dim=1)
+        print("tar clean sim: ", tar_clean_sim)
+        print("adv tar sim: ", adv_tar_sim)
+        fitness_ = adv_tar_sim - tar_clean_sim
         return fitness_
     
     def text_in_benchmark(self, pop, position):
